02785bf626b70c30750eeee38607338c54167a8d
Amira <amyami187@gmail.com>
2019-11-26 11:30:34 -0500

    Update featuremap.py

--- modules/featuremap.py
+++ modules/featuremap.py
@@ -6,23 +6,23 @@
 
 import pennylane as qml
 
-"""def featuremap(x1, x2, y, phi0):
+def featuremap(x1, x2, y, phi0):
     # encode y label
     if y == 1:
-        qml.CNOT(wires=[4, 5])  # flip label qubit
-    qml.CRot(phi0.item(0), phi0.item(1), phi0.item(2), wires=[6, 3])
+        qml.CNOT(wires=[5, 4])  # flip label qubit
+    qml.CRot(phi0(0), phi0(1), phi0(2), wires=[4, 2])
+    qml.CRot(x1, x2, 0, wires=[4, 2])
+    qml.CRot(phi0(3), phi0(4), phi0(5), wires=[4, 3])
     qml.CRot(x1, x2, 0, wires=[4, 3])
-    qml.CRot(phi0.item(3), phi0.item(4), phi0.item(5), wires=[6, 4])
-    qml.CRot(x1, x2, 0, wires=[6, 4])
-    qml.CZ(wires=[3, 4])
-    qml.CZ(wires=[4, 3])
+    qml.CZ(wires=[2, 3])
+    qml.CZ(wires=[3, 2])
     # layer 2
-    qml.CRot(phi0.item(6), phi0.item(7), phi0.item(8), wires=[6, 3])
-    qml.CRot(x1, x2, 0, wires=[6, 3])
-    qml.CRot(phi0.item(9), phi0.item(10), phi0.item(11), wires=[6, 4])
-    qml.CRot(x1, x2, 0, wires=[6, 4])
-    qml.CZ(wires=[3, 4])
-    qml.CZ(wires=[4, 3])
+    qml.CRot(phi0(6), phi0(7), phi0(8), wires=[4, 2])
+    qml.CRot(x1, x2, 0, wires=[4, 2])
+    qml.CRot(phi0(9), phi0(10), phi0(11), wires=[4, 3])
+    qml.CRot(x1, x2, 0, wires=[4, 3])
+    qml.CZ(wires=[2, 3])
+    qml.CZ(wires=[3, 2])
 """
 def featuremap(x1, x2, y, phi0):
     # encode y label
@@ -36,4 +36,4 @@ def featuremap(x1, x2, y, phi0):
     qml.CRX(x2, wires = [4,3])
     qml.CRX(phi0[2], wires = [4,2])
     qml.CRX(phi0[3], wires = [4,3])
-
+"""
