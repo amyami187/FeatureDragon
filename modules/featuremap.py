15710db23b1d82aa095332af19ea0207f6fc7c4d
Amira <amyami187@gmail.com>
2019-11-26 11:40:06 -0500

    Update featuremap.py

--- modules/featuremap.py
+++ modules/featuremap.py
@@ -10,16 +10,16 @@ def featuremap(x1, x2, y, phi0):
     # encode y label
     if y == 1:
         qml.CNOT(wires=[5, 4])  # flip label qubit
-    qml.CRot(phi0(0), phi0(1), phi0(2), wires=[4, 2])
+    qml.CRot(phi0[0], phi0[1], phi0[2], wires=[4, 2])
     qml.CRot(x1, x2, 0, wires=[4, 2])
-    qml.CRot(phi0(3), phi0(4), phi0(5), wires=[4, 3])
+    qml.CRot(phi0[3], phi0[4], phi0[5], wires=[4, 3])
     qml.CRot(x1, x2, 0, wires=[4, 3])
     qml.CZ(wires=[2, 3])
     qml.CZ(wires=[3, 2])
     # layer 2
-    qml.CRot(phi0(6), phi0(7), phi0(8), wires=[4, 2])
+    qml.CRot(phi0[6], phi0[7], phi0[8], wires=[4, 2])
     qml.CRot(x1, x2, 0, wires=[4, 2])
-    qml.CRot(phi0(9), phi0(10), phi0(11), wires=[4, 3])
+    qml.CRot(phi0[9], phi0[10], phi0[11], wires=[4, 3])
     qml.CRot(x1, x2, 0, wires=[4, 3])
     qml.CZ(wires=[2, 3])
     qml.CZ(wires=[3, 2])
