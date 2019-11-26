c11af31e0cb95b83e44c9a4099eef6cc3076694f
Amira <amyami187@gmail.com>
2019-11-26 09:17:49 -0500

    Update featuremap.py

--- modules/featuremap.py
+++ modules/featuremap.py
@@ -9,6 +9,8 @@ def featuremap(x1, x2, y, phi0):
     # encode y label
     if y == 1:
         qml.CNOT(wires=[5, 4])  # flip label qubit
-    qml.RX(phi0[0], wires = [2])
-    qml.RX(phi0[1], wires = [3])
+    qml.CRX(x1, wires = [4,2])    
+    qml.CRX(x2, wires = [4,3])
+    qml.CRX(phi0[0], wires = [4,2])
+    qml.CRX(phi0[1], wires = [4,3])
 
